f10bed72b3d811027721e8f0014e8430c0b374d8
Mike Bitfusion <mike@bitfusion.trade>
2025-04-16 13:58:54 +0800

    BingX orderbook

--- parser/interfaces/bingx.py
+++ parser/interfaces/bingx.py
@@ -13,6 +13,14 @@ class FundingRate:
     nextFundingTime: datetime
     lastPrice: Decimal
 
+@dataclass
+class OrderBook: 
+    symbol: str
+    bestBidPrice: Decimal
+    bestBidQuote: Decimal
+    bestAskPrice: Decimal
+    bestAskQuote: Decimal
+
 
 BASE_URL = "https://open-api.bingx.com"
 
@@ -36,4 +44,31 @@ async def get_funding_rate():
             )
             for row in response.json()["data"]
         ]
+    
+@retry(catch_exceptions=(TransportError,))
+async def get_orderbook(symbol: str, limit: 5):
+    """
+    https://bingx-api.github.io/docs/#/en-us/swapV2/market-api.html#Order%20Book
+    """
+    
+    endpoint = BASE_URL + "/openApi/swap/v2/quote/depth"
+    params = { "symbol": symbol, "limit": limit }
+    async with AsyncClient() as client:
+        response = await client.get(endpoint, params=params)
+        if response.status_code != 200:
+            raise ValueError(f"Error fetching orderbook: {response.text}")
+        data = response.json()
+        if data.get("code") != 0:
+            raise ValueError(f"API error: {data.get('msg')}")
+        ob_data = data["data"]
+        best_bid = ob_data["bids"][0]
+        best_ask = ob_data["asks"][0]
+        return OrderBook(
+            symbol=symbol,
+            bestBidPrice=Decimal(best_bid[0]),
+            bestBidQuote=Decimal(best_bid[0])*Decimal(best_bid[1]),
+            bestAskPrice=Decimal(best_ask[0]),
+            bestAskQuote=Decimal(best_ask[0])*Decimal(best_ask[1])
+        )
+
     
